d3c1d75fdea7533fad8f5a6d3e881e10724c726e
kasettakorn <kasettakorn_techno@hotmail.com>
2020-10-13 13:44:28 +0700

    Add HTBF countdown time

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Countdown from './Countdown';
 import HTBFbanner from './images/hacktober-13.png';
 export default class App extends Component {
   render() {
     return (
       <div className="App">
         <img className="htbf-banner" src={HTBFbanner} alt="htbf-banner" />
+        <Countdown />
       </div>
     )
   }
